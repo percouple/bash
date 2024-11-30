4c13f6d5e41fc208ec352e63c99cb7431c201281
Caleb Evans <calebsevan@gmail.com>
2024-11-30 11:40:25 -0600

    Validate copy directory

--- Photos-backup-script/photos-backup-script.sh
+++ Photos-backup-script/photos-backup-script.sh
@@ -12,9 +12,20 @@ if [ ! -d "$PHOTOS_LIBRARY_PATH" ]; then
 fi
 
 # Check if the copy directory exists
-read -p "Please enter copy directory: " -t 3600 COPY_DIRECTORY
+read -p "Please enter copy directory path: " -t 3600 COPY_DIRECTORY
 
-echo $COPY_DIRECTORY
+# Check if user entered a directory
+if [ -z "$COPY_DIRECTORY" ]; then
+    echo "No input received within the timeout period."
+    exit 1
+fi
+
+# Check if the directory exists
+if [ -d "$COPY_DIRECTORY" ]; then
+    echo "The directory '$COPY_DIRECTORY' exists."
+else
+    echo "The directory '$COPY_DIRECTORY' does not exist."
+fi
 
 # Wait for the specified interval before checking again
 # sleep $INTERVAL_SECONDS
