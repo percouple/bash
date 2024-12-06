1ecc7bb5f32506eafcbbf1daeed91a00bd0becd0
percouple <calebsevan@gmail.com>
2024-12-06 15:34:44 -0600

    post pull

--- backupFiles.sh
+++ backupFiles.sh
@@ -1,4 +1,17 @@
-
+#!/bin/bash
 # Take in file, copy relevant files over
 # Filter by file type
-echo 'Script is running, bro'
+
+echo Script is running, bro
+
+# Ask for input file
+# Detect whether input file exists
+    # Re-prompt while it does not
+
+# Ask for destination file
+
+# Scan for files in input file 
+
+# 
+
+echo "This is your input file: $inputFile" 
