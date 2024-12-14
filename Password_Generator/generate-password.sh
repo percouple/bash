6378429fdcb8bc3393ba64eae51b0816d7541dc9
Caleb Evans <calebsevan@gmail.com>
2024-12-14 09:02:31 -0600

    finish execution of random chars

--- Password_Generator/generate-password.sh
+++ Password_Generator/generate-password.sh
@@ -3,13 +3,17 @@
 # Establish length var
 LENGTH=1
 
+# Prompt user for length
 echo "How many chars would you like your password to be?"
+read LENGTH
 
 # Loop until user inputs a sufficient length
-while [ $LENGTH -le 8 ]
+while [[ $LENGTH -le 8 || $LENGTH -ge 26 ]]
 do
-echo "Err: Passwords must be a minimum length of 8."
+echo "Err: Passwords must be between 8 and 26 chars."
 read LENGTH
 done
 
-echo $LENGTH
+echo "
+Here you go! "
+head -c $LENGTH /dev/urandom | base64 | tr -dc 'A-Za-z0-9@#$%^&*-_,.?' | head -c $LENGTH
