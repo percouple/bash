a41cda56440e0f199e153008b5b5cfbab0eb9a73
Caleb Evans <calebsevan@gmail.com>
2024-12-26 10:49:37 -0600

    randomizer function implemented

--- File_Order_Randomizer/script.sh
+++ File_Order_Randomizer/script.sh
@@ -14,3 +14,9 @@ if [ ! -d "$DIR" ]; then
   echo "Error: $DIR is not a valid directory."
   exit 1
 fi
+
+echo -e "\nThis is the directory that will be permanently changed: \n"$DIR
+
+FILES=($(find $DIR -maxdepth 1 -type f | sort -R))
+
+echo "Files Randomized: " ${FILES[*]}
